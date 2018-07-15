0022fc8b8df9316788dbbcdbe439538138531626
Jessica Long <jessicalong627@gmail.com>
2018-07-15 15:21:00 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,22 +1,22 @@
-
-+def reformat_languages(languages)
-+  new_hash = {}
-+  languags.each do |language_cat, languages|
-+    languages.each do |language, attributes|
-+      attributes.each do |attribute, att_value|
-+        if language == :javascript
-+          new_hash[language] = {
-+            :type => att_value,
-+            :style => [:oo, :functional]
-+          }
-+        else
-+          new_hash[language] = {
-+             :type => att_value,
-+             :style => [language_cat]
-+          }
-+        end
-+      end
-+    end
-+  end
-+  return new_hash
- end	 end
+def reformat_languages(languages)
+  new_hash = {}
+  languags.each do |language_cat, languages|
+    languages.each do |language, attributes|
+      attributes.each do |attribute, att_value|
+        if language == :javascript
+          new_hash[language] = {
+            :type => att_value,
+            :style => [:oo, :functional]
+          }
+        else
+          new_hash[language] = {
+             :type => att_value,
+             :style => [language_cat]
+          }
+        end
+      end
+    end
+  end
+  return new_hash
+ end	 
+end
